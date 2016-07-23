1dfb459dbb80b435383158d2868d795a41fdc031
Rodrigo Lima <rodrigo.lima@zethasolutions.com>
2016-07-23 12:19:45 -0300

    arquivo de variaveis

---
+++ variaveis.js
@@ -0,0 +1,20 @@
+var varInt;
+//"strings" - podem ser usadas para conter código dentro, como programação dentro da string
+//'literais' - contém apenas textos
+
+varInt = 1
+console.log('VarInt: ', varInt)
+
+varInt = 2
+console.log('VarInt: ', varInt)
+
+varInt = 3
+console.log('VarInt: ', varInt)
+
+var varFloat = 2.5
+console.log('varFloat: ', varFloat)
+
+console.log('Minha soma: ', varFloat+varInt);
+
+var varstring = 'texto qualquer'
+console.log('textos qualquer',varstring)
