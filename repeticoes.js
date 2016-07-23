4b642ddfc70f88ca0eb1640bd2f99ed36502afe3
Rodrigo Lima <rodrigo.lima@zethasolutions.com>
2016-07-23 13:51:03 -0300

    add repeticoes

---
+++ repeticoes.js
@@ -0,0 +1,14 @@
+//repetições while
+var i = 1
+
+while(i<=10)
+{
+  console.log('estou dentro do while',i)
+  i++
+}
+
+// repetição for
+for(var x = 1;x<=10; x++)
+{
+  console.log('estou dentro do for',x)
+}
