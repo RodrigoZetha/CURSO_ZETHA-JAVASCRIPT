b8a40415a670c6097a0770f7910bcd24d2db5698
Rodrigo Lima <rodrigo.lima@zethasolutions.com>
2016-07-23 13:15:30 -0300

    add logica.js

---
+++ logica.js
@@ -0,0 +1,17 @@
+var t = true
+var f = false
+
+
+//operador diferente '!='
+//testando verdadeiros
+t= 1==1
+t= 1=='1'
+console.log(t)
+
+
+t= 1==='1'
+console.log(f)
+
+
+// operador 'AND' &&
+// operador 'OR' ||
